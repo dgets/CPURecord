8dcd7d9103bb7ae31dd53e2050520ceb37de7cfa
Damon Getsman <damo.gets@gmail.com>
2017-10-18 20:21:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/khelair/cpurecord/RecordUsage.java
@@ -0,0 +1,18 @@
+package com.example.khelair.cpurecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RecordUsage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_usage);
+    }
+
+    public void onManualRecordClick(View view) {
+
+    }
+}
